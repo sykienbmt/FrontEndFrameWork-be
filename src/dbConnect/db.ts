d5fd387d0fe7de5842be0c6abb60cb03d05b1eb1
Starr <sikien1995@gmail.com>
2022-03-04 16:27:06 +0700

    db deploy

--- src/dbConnect/db.ts
+++ src/dbConnect/db.ts
@@ -1,20 +1,20 @@
 
 import { Pool } from 'pg';
 
-export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'postgres',
-    database: 'assignment',
-    port: 5432
-});
-
 // export const pool = new Pool({
 //     user: 'postgres',
-//     host: 'containers-us-west-11.railway.app',
-//     password: 'ZG2O3b90yo3FX3rAYgil',
-//     database: 'railway',
-//     port: 6015
+//     host: 'localhost',
+//     password: 'postgres',
+//     database: 'assignment',
+//     port: 5432
 // });
+
+export const pool = new Pool({
+    user: 'postgres',
+    host: 'containers-us-west-11.railway.app',
+    password: 'ZG2O3b90yo3FX3rAYgil',
+    database: 'railway',
+    port: 6015
+});
 
 
